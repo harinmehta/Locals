7b99fb52aeb48d69c196c87312f65d7e7fd888b2
harinmehta <harinmehta17@gmail.com>
2020-05-03 11:34:07 +0530

    first commit

---
+++ src/main/java/com/locals/InternshipTestApplication.java
@@ -0,0 +1,13 @@
+package com.locals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locals/InternshipTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
